2b92ad1637a7776aae88e11560ee158e375bcabf
JemDAW <jemimamolina.alu@iespacomolla.es>
2020-11-02 21:42:49 +0100

    Practica Completada

--- form.js
+++ form.js
@@ -4,7 +4,25 @@ var dni = document.getElementById("dni");
 dni.addEventListener("input", validarDNI, false);
 
 function validarDNI(){
+    var dniSinLetra= dni.value.slice(0,-1);
+    var letraDni= dni.value.slice(-1);
 
+    if(esCorrecto(dniSinLetra) && calcularLetra(dniSinLetra) == letraDni){
+        dni.setCustomValidity("");
+    }else{
+        dni.setCustomValidity("La letra no es correcta");
+    }
+}
+
+function esCorrecto(dni){
+    return /^(\d{8})$/.test(dni);
+   }
+
+function calcularLetra(dni){
+    let secuenciaLetras= "TRWAGMYFPDXBNJZSQVHLCKE";
+    let num_letra= dni % 23;
+    let letra = secuenciaLetras[num_letra];
+    return letra;
 }
 
 //2
@@ -45,12 +63,15 @@ function esAnagrama(text1, text2){
   }
 
 //4
+var form4= document.getElementById("form4");
+var botEnviar= document.getElementById("botForm4");
 var select1= document.getElementById("sel1");
+var select2= document.getElementById("sel2");
 
 select1.addEventListener("input", hacerSegundoSelect,false);
+botEnviar.addEventListener("click", enviar,false);
 
 function hacerSegundoSelect(){
-    let select2= document.getElementById("sel2");
 
     select2.innerHTML= "<option>No seleccionado</option>";
 
@@ -60,21 +81,55 @@ function hacerSegundoSelect(){
 
     if(select1.value == 'a'){
         op1.innerText = "Alicante";
+        op1.value = 'a';
+
         op2.innerText = "Elche";
+        op2.value = 'b';
+
         op3.innerText = "Petrer";
+        op3.value = 'c';
     }else if(select1.value == 'c'){
         op1.innerText = "Castellón";
+        op1.value = 'a';
+
         op2.innerText = "Oropesa";
+        op2.value = 'b';
+
         op3.innerText = "Vinaroz";
+        op3.value = 'c';
     }else{
         op1.innerText = "Valencia";
+        op1.value = 'a';
+
         op2.innerText = "Xativa";
+        op2.value = 'b';
+
         op3.innerText = "Torrent";
+        op3.value = 'c';
     }
 
     select2.appendChild(op1);
     select2.appendChild(op2);
     select2.appendChild(op3);
+}
 
+function enviar(){
+    comprobarSelects();
+    if (form4.checkValidity()){
+        form4.submit();
+    }
+}
 
+function comprobarSelects(){
+    if (select1.value == 'a' || select1.value == 'c' || select1.value == 'v'){
+        select1.setCustomValidity("");
+    }else{
+        select1.setCustomValidity("Debes seleccionar una opción válida");
+    }
+
+    if(select2.value == 'a' || select2.value == 'b' || select2.value == 'c'){
+        select2.setCustomValidity("");
+    }else{
+        select2.setCustomValidity("Debes seleccionar una opción válida");
+    }
 }
